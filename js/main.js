c0bd2b8cb2e6fe6434013b8efbc4acc768e752d1
Amir Hafizovic <hafizovic.amir@gmail.com>
2017-12-21 20:48:08 +1100

    added players

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ console.log('connected');
 
 //loop for creating array grid
 let columns = 3, rows = 3;
-let grid = [];
-for(let y = 0; y < rows; y++)
-{
-    grid.push([]);
-    for(let x = 0; x < columns; x++)
-    {
-        grid[y].push(0);
-    }
+
+const initGrid = function() {
+  let grid = [];
+  for(let y = 0; y < rows; y++) {
+      grid.push([]);
+      for(let x = 0; x < columns; x++) {
+          grid[y].push(0);
+      }
+  }
+  return grid;
 };
-//let columns = 3, rows = 3
+
 //function which generates html table grid
-function createGrid(columns, rows) {
+const createGrid = function(rows, columns) {
   const table = $('.table');
 
   for (let i = 0; i < rows; i++) {
@@ -26,31 +28,54 @@ function createGrid(columns, rows) {
       row.append(cell);
     }
   }
+}
+
+const getPlayerName = function() {
+  const name1 = prompt(`Player X's name?`);
+  const name2 = prompt(`Player O's name?`);
+  return [name1, name2];
+}
+
+const getPlayers = function() {
+  const names = getPlayerName();
+  const playerX = { name: names[0], marker: 'X', score: 0 };
+  const playerO = { name: names[1], marker: 'O', score: 0 };
+
+  return [playerX, playerO];
 };
+
+const findPlayerByMarker = function(players, marker) {
+  for(let i = 0; i < players.length; i++) {
+    if (players[i].marker === marker) {
+      return players[i];
+    }
+  }
+}
+
 //MATCHING LOGIC
-const match = function(marker){
+const match = function(grid, marker){
   // Rows check
   for (let i = 0; i < rows; i++) {
     if (grid[i][0] === marker && grid[i][1] === marker && grid[i][2] === marker) {
       console.log('matched');
-      return alert(`Player '${marker}' wins!`);
+      return marker;
     }
   }
   // Columns check
   for (let j = 0; j < columns; j++) {
     if (grid[0][j] === marker && grid[1][j] === marker && grid[2][j] === marker) {
       console.log('matched');
-      return alert(`Player '${marker}' wins!`);
+      return marker;
     }
   }
   // Diagonals check
   if (grid[0][0] === marker && grid[1][1] === marker && grid[2][2] === marker) {
     console.log('matched');
-    return alert(`Player '${marker}' wins!`);
+    return marker;
   }
   if (grid[0][2] === marker && grid[1][1] === marker && grid[2][0] === marker) {
     console.log('matched');
-    return alert(`Player '${marker}' wins!`);
+    return marker;
   }
 };
 // checks to see if array contains 0's
@@ -61,44 +86,54 @@ const isFinished = function(grid) {
   return true;
 };
 
+// initialises game plus dependencies
+const startGame = function() {
+  let grid = initGrid();
+  createGrid(rows, columns);
+  const players = getPlayers();
+  const playerX = players[0];
+  const playerO = players[1];
 
-$( document ).ready(function() {
-  createGrid(columns, rows);
-
-  //thanks milo for odd/even concept /// Front end alternating markers + markers being assigned to array coordinates through matching td data attributes
+  // Thanks milo for odd/even concept
   let count = 0;
   let marker;
-  const dataHandler = function() {
 
+  const dataHandler = function() {
 
     const $row = $(this).attr('data-row');
     const $col = $(this).attr('data-column');
     if ($(this).is(':empty')) {
 
       if (count % 2 === 0) {
-        marker = 'X';
+        marker = playerX.marker;
       }
-      else marker = 'O';
+      else marker = playerO.marker;
+
       const $Click = $(this).html(`<p class="input">${marker}</p>`);
       grid[$row][$col] = marker;
       console.log(`html-data:${$row},${$col} Marker: ${marker}`)
       count++;
     };
     // Winner or draw notification logic
-    const winner = match(marker);
+    const winner = match(grid, marker);
 
     if (winner) {
-      alert(`player ${winner} has won`);
-      return;
-      // restart the game
+      const winningPlayer = findPlayerByMarker(players, winner);
+      alert(`${winningPlayer.name} has won!`);
+      $('tr').remove();
+      grid = initGrid();
+      createGrid(rows, columns);
+      $('.playerX').html(`<p>${playerX.name}: ${playerX.score}</p>`);
+      $('.playerO').html(`<p>${playerO.name}: ${playerO.score}</p>`);
     }
     else if (!winner && isFinished(grid)) {
       alert(`It's a draw!`);
-      return;
-      // restart the game
+      $('tr').remove(); // clears gameboard html markup
+      grid = initGrid(); // regenerates game array
+      createGrid(rows, columns); //re-creates html gameboard
     }
-
   };
   $('.table').on('click', 'td', dataHandler);
+};
 
-});
+$( document ).ready(startGame);
